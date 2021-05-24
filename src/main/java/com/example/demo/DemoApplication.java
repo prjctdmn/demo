eccb33a9307dae1e4a15aa6cba587404e14af3c7
prjctdmn <prjctdmn@gmail.com>
2021-05-24 21:27:46 +0530

    sysout removed

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -15,8 +15,5 @@ public class DemoApplication {
     @PostConstruct
     public void printMessage() {
 		//System.out.println("Hi");
-	    System.out.println("One more sysout added");
-	    
     }
-
 }
