53325bbf350a92cd50ea2c49c43e1b5c9b2a39c4
prjctdmn <prjctdmn@gmail.com>
2021-05-24 20:39:38 +0530

    Sysout Added

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,11 +3,18 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @PostConstruct
+    public void printMessage() {
+		System.out.println("Hi");
+    }
 
 }
